f7d49c6aab37fc2eb1e19aec0db9182428543401
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2023-11-01 11:30:17 +0530

    Using mean() function to fill incomplete rows

---
+++ Replace/Mean.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+x = pd.read_csv("D:\Coding\Pandas\Replace\Courses.csv")
+
+y = x["Seats_remaining"].mean()
+
+x.index = np.arange(1, len(x) + 1)
+
+z = x["Seats_remaining"].fillna(y,inplace=True) ## Replacing value for specified column 
+
+print("\n",x.to_string())
